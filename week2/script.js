c97597506851c22d7cff235563190363230500b3
Gunjan Dhawan <51229240+GdhawanG@users.noreply.github.com>
2022-06-28 16:33:32 +0530

    Added null check for Password & Confirm Password field

--- week2/script.js
+++ week2/script.js
@@ -1,5 +1,12 @@
 function checkPassword() {
   var pass = document.getElementById("pass").value;
   var passConfirm = document.getElementById("passConfirm").value;
-  pass === passConfirm ? alert("pass match") : alert("pass not match");
+
+  if (pass == "" || passConfirm == "") {
+    alert("enter some value first");
+  } else if (pass === passConfirm) {
+    alert("pass match");
+  } else {
+    alert("pass not match");
+  }
 }
