498d959fbaa4500200be04cd7e0a52407883a185
Gunjan Dhawan <gunjandhawan235@gmail.com>
2022-07-04 18:29:39 +0530

    Updated readme & script

--- week3/README.md
+++ week3/README.md
@@ -2,11 +2,4 @@
 
 Week 3 assignments.
 
-# Call, Apply, Bind
-
-1. call() :
-   The call() allows for a function or method belonging to one object to be assigned and called for a different object. You can refer to the values of the object by using the this keyword.
-2. apply() :
-   The apply() function works similar to call() function. The only difference between the call() and apply() function is you can pass multiple parameters in the array and use them.
-3. bind() :
-   The bind() returns the exact copy of a function and binds it with an object. This method is used to bind and keep a copy of a method and use it later. You can use the function whenever you want by invoking it.
+# Closures
--- week3/script.js
+++ week3/script.js
@@ -1,31 +1,16 @@
-let obj = {
-  firstName: "John",
-  lastName: "Doe",
-  getFullName: function () {
-    console.log(this.firstName + " " + this.lastName);
-  },
-  getMethodDetails: function (sNo, methodName) {
-    console.log(sNo + " " + methodName);
-  },
-};
-obj.getFullName(); // John Doe
-obj.getMethodDetails(18, "Male"); // 18 Male
-
-let obj2 = {
-  firstName: "Johny",
-  lastName: "Bravo",
-};
-
-// 1. call() in action
-obj.getFullName.call(obj2); // Johny Bravo
-obj.getMethodDetails.call(obj2, 1, "call"); // 1 call
-
-// 2. apply() in action
-obj.getFullName.apply(obj2); // Johny Bravo
-obj.getMethodDetails.apply(obj2, [2, "apply"]); // 2 apply
-
-// 3. bind() in action
-let bindNameFunc = obj.getFullName.bind(obj2);
-bindNameFunc(); // Johny Bravo
-let bindMethodFunc = obj.getMethodDetails.bind(obj2);
-bindMethodFunc(3, "bind"); // 3 bind
+function createIncrement() {
+  let count = 0;
+  function increment() {
+    count++;
+  }
+  let message = `Count is ${count}`;
+  function log() {
+    console.log(message);
+  }
+  return [increment, log];
+}
+const [increment, log] = createIncrement();
+increment();
+increment();
+increment();
+log(); // What is logged?
