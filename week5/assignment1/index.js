ce1a4b5776631af5b2301ed5083452439217b693
Gunjan Dhawan <gunjandhawan235@gmail.com>
2022-07-17 10:15:17 +0530

    async-await functions created

---
+++ week5/assignment1/index.js
@@ -0,0 +1,36 @@
+const doTask1 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Task 1");
+    }, Math.random() * 1000);
+  });
+};
+const doTask2 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Task 2");
+    }, Math.random() * 1000);
+  });
+};
+const doTask3 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Task 3");
+    }, Math.random() * 1000);
+  });
+};
+
+const awaitFunc = async () => {
+  try {
+    const response1 = await doTask1();
+    console.log(response1);
+    const response2 = await doTask2();
+    console.log(response2);
+    const response3 = await doTask3();
+    console.log(response3);
+  } catch (error) {
+    console.error(`async await error ${error}`);
+  }
+};
+
+awaitFunc();
