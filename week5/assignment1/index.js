f4e26228ef247c6df6ed2303e69bac711887a985
Gunjan Dhawan <gunjandhawan235@gmail.com>
2022-07-25 17:50:26 +0530

    generators implemented

--- week5/assignment1/index.js
+++ week5/assignment1/index.js
@@ -2,6 +2,7 @@ const doTask1 = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       res("Task 1");
+      rej("No task 1");
     }, Math.random() * 1000);
   });
 };
@@ -9,6 +10,7 @@ const doTask2 = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       res("Task 2");
+      rej("No task 2");
     }, Math.random() * 1000);
   });
 };
@@ -16,6 +18,7 @@ const doTask3 = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       res("Task 3");
+      rej("No task 3");
     }, Math.random() * 1000);
   });
 };
@@ -33,16 +36,22 @@ const awaitFunc = async () => {
   }
 };
 
-// awaitFunc();
+awaitFunc();
 
 function* myGenerator() {
-  // console.log("in myGenerator function");
-  yield doTask1();
-  yield doTask2();
-  yield doTask3();
+  yield doTask1()
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err));
+  yield doTask2()
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err));
+  yield doTask3()
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err));
 }
 
-console.log(myGenerator().next());
-console.log(myGenerator().next());
-console.log(myGenerator().next());
-console.log(myGenerator().next());
+const gen = myGenerator();
+gen.next();
+gen.next();
+gen.next();
+gen.next();
