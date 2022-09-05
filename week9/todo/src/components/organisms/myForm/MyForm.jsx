039ed6df63e71669a04bc2778ee78e44e6c7ce64
Gunjan Dhawan <gunjandhawan235@gmail.com>
2022-09-05 16:38:43 +0530

    added propTypes for MyButton, passed from MyForm

--- week9/todo/src/components/organisms/myForm/MyForm.jsx
+++ week9/todo/src/components/organisms/myForm/MyForm.jsx
@@ -1,10 +1,13 @@
 import InputWithHeader from "../../molecules/InputWithHeader/InputWithHeader";
 import MyButton from "../../atoms/myButton/MyButton";
+
 const MyForm = () => {
   return (
     <>
       <InputWithHeader />
-      <MyButton />
+      <MyButton variant="contained" size="small">
+        Submit
+      </MyButton>
     </>
   );
 };
