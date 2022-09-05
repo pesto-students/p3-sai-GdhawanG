0c4403b3763839f86af197c4c8bc388a5470f486
Gunjan Dhawan <gunjandhawan235@gmail.com>
2022-09-05 09:56:46 +0530

    Implementing Atomic Design : In Progress

---
+++ week9/todo/src/components/atoms/myButton/MyButton.jsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+const MyButton = () => {
+  return (
+    <Button variant="primary mb-3" type="error">
+      Submit
+    </Button>
+  );
+};
+
+export default MyButton;
---
+++ week9/todo/src/components/organisms/myForm/MyForm.jsx
@@ -0,0 +1,12 @@
+import InputWithHeader from "../../molecules/InputWithHeader/InputWithHeader";
+import MyButton from "../../atoms/myButton/MyButton";
+const MyForm = () => {
+  return (
+    <>
+      <InputWithHeader />
+      <MyButton />
+    </>
+  );
+};
+
+export default MyForm;
